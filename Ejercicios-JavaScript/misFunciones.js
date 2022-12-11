3a5e8d0a4afa54c9c8885ef4eb11a99d93bf73fa
LeonardoMendezR <2108727@ucc.edu.ar>
2022-12-11 18:45:33 -0300

    entrega de ejercicios

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -115,3 +115,114 @@ function cargarResultado(){
     un= urlComp.split("#")[2];
     document.getElementById("dist").value= cant + " "+un;
 }
+function guardarlocalstorage(){
+    let distancia,unidad;
+    distancia=document.getElementById('distancia').value;
+    unidad=document.getElementsByName('unidades')[0].value;
+    localStorage.setItem("distanciaLS",distancia);
+    localStorage.setItem("unidadesLS",unidad);
+
+    window.open('segundaWeb.html');
+}
+function cargarlocalstorage(){
+    let cant,un;
+    cant=localStorage.getItem("distanciaLS");
+    un=localStorage.getItem("unidadesLS");
+
+    document.getElementById("dist").value=cant+ " "+ un;
+}
+function dibujarCC(){
+    var canvas= document.getElementById("myCanvas");
+    var ctx=canvas.getContext("2d");
+    var yMAx= canvas.height;
+    var xMAx= canvas.width;
+    var margen=5;
+    ctx.fillStyle="#35555";
+    ctx.fillRect(0+margen,yMAx-40-margen,40,40);
+
+
+    ctx.arc(xMAx/2,yMAx/2,40,0,2*Math.PI);
+    ctx.stroke();
+    ctx.fill();
+}
+var bandera;
+function dibujar(event) {
+    var canvas = document.getElementById("canvasAdibujar");
+    var ctx =canvas.getContext("2d");
+    var posX=event.clientX;
+    var posY=event.clientY;
+    console.log(posX, posY);
+    canvas.onmousedown=function(){bandera=true};
+    canvas.onmouseup=function(){bandera=false};
+    if(bandera){
+        ctx.fillRect(posX,posY,5,5);
+        ctx.fill;
+    
+    }
+}
+function limpiarCanvas(){
+    
+    var canvas = document.getElementById("canvasAdibujar");
+    var ctx =canvas.getContext("2d");
+
+    canvas.width = canvas.width;
+}
+function dibujarcuadriculado(){
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    var anchoMax= canvas.width;
+    var alturaMax=canvas.height;
+    //dibujar lineas horizontales
+    ctx.beginPath();
+    for(var i=0;i<alturaMax;){
+        ctx.moveTo(0,i);
+        ctx.lineTo(1000,i);
+        ctx.strokestyke="#3e67d9";
+        ctx.stroke();
+        i=i+20;
+    }
+    ctx.closePath();
+    //dibujar lineas verticales
+    ctx.beginPath();
+    
+    for(var i=0;i<anchoMax;){
+        ctx.moveTo(i,0);
+        ctx.lineTo(i,1000);
+        ctx.strokestyke="#3e67d9";
+        ctx.stroke();
+        i=i+20;
+    }
+    ctx.closePath();
+    
+}
+function dibujarImagen(posX, posY){
+    var canvas= document.getElementById("myCanvas");
+    var ctx= canvas.getContext("2d");
+    console.log(posX,posY);
+    var img=new Image();
+    img.src="images/auto.png";
+    canvas.width=canvas.width;
+    img.onload=function(){
+        ctx.drawImage(img,posX,posY);
+    }
+}
+x=0;
+dx=2;
+function animarAuto(){
+    
+    var canvas= document.getElementById("myCanvas");
+    var ctx= canvas.getContext("2d");
+    
+    canvas.width=canvas.width;
+
+    var img=new Image();
+    img.src="images/auto.png";
+    
+    img.onload=function(){
+        ctx.drawImage(img,x,100);
+    }
+    if(x>canvas.width){
+        x=0;
+    }
+    x+=dx;
+}
